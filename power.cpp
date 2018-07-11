43ba3b4ea799b4bb9ad3e7a4898eb7e2ab74cd5f
Khodeeva <xodeeva98@bk.ru>
2018-07-11 11:30:01 +0300

    MashaChans

--- power.cpp
+++ power.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 int main()
 {
-int b=2;
-for(int i=0;i<10;++i)
-{
-int p;
-p=1;
-for(int t=1;t<=i;++t)
-p=p*b;
-std::cout<<p<<" ";
-}
-std::cout<<std::endl;
-return 0;
+	int b_number=2; 
+	for(int i_tcical=0;i_tcical<10;++i_tcical)
+	{
+	int p_this_number;
+	p_this_number=1;
+	for(int t=1;t<=i;++t)
+	p_this_number=p*b_number;
+	std::cout<<p_this_number<<" ";
+	}
+	std::cout<<std::endl;
+	return 0;
 }
